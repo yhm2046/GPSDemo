0c44debf9c6ad922cb4b157301344330249841a3
evan <evan@123.com>
2022-10-26 17:36:05 +0800

    update gradle version

---
+++ app/src/main/java/com/adan/gpsdemo/TestFusedLocationProviderClientActivity.java
@@ -0,0 +1,13 @@
+package com.adan.gpsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestFusedLocationProviderClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
